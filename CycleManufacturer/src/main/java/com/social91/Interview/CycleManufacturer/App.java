748fb29d819844bb1af8cdc5267040b9461a757f
gkumar38 <35846630+gkumar38@users.noreply.github.com>
2020-07-25 12:41:32 +0530

    Coding Interview

---
+++ CycleManufacturer/src/main/java/com/social91/Interview/CycleManufacturer/App.java
@@ -0,0 +1,13 @@
+package com.social91.Interview.CycleManufacturer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

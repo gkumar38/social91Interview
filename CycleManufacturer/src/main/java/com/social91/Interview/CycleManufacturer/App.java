b4398d29b97a6403ae5ff56a6802a60616314469
gkumar38 <35846630+gkumar38@users.noreply.github.com>
2020-07-25 15:43:28 +0530

    Update App.java

--- CycleManufacturer/src/main/java/com/social91/Interview/CycleManufacturer/App.java
+++ CycleManufacturer/src/main/java/com/social91/Interview/CycleManufacturer/App.java
@@ -1,28 +1,38 @@
 package com.social91.Interview.CycleManufacturer;
 
-import java.sql.Timestamp;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Month;
-import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 
-/**
- * Hello world!
- *
- */
+
 public class App implements CycleCommonAttribute
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws ParseException
     {
-        
-        
-        
+    	 App ap = new App();
+    	 BlockingQueue<EvaluateCost> queue = new ArrayBlockingQueue<>(1000);
+    	 ChainAssembly chainAssembly = new ChainAssembly(queue);
+    	 Wheels wheels = new Wheels(queue);
+    	 Seating seat = new Seating(queue);
+    	 Frame frame = new Frame(queue);
+    	 HandleBar hbr = new HandleBar(queue);
+    	 new Thread((Runnable) frame).start();
+         new Thread((Runnable) seat).start();
+         new Thread((Runnable) wheels).start();
+         new Thread((Runnable) chainAssembly).start();
+         new Thread((Runnable) hbr).start();
+         long diff = ap.evaluateTime();
+         if(diff>9) {
+        	 System.out.println(ap.evaluatePrice());
+         }
     }
 
 	@Override
-	public void evaluateTime() throws ParseException {
+	public long evaluateTime() throws ParseException {
 		
 		String startTime = "01/01/2016";
 		Date endTime = new Date();
@@ -36,20 +46,26 @@ public class App implements CycleCommonAttribute
 		if(diff>9) {
 			startTime = end;
 		}
+		return diff;
 	}
 
 	@Override
 	public double evaluatePrice() {
-		ChainAssembly chain = new ChainAssembly();
+		ChainAssembly chain = null;
+		@SuppressWarnings("null")
 		double chainPrice = chain.priceEvaluation();
-        Frame fr = new Frame();
-        double framePrice = fr.priceEvaluation();
-        HandleBar hbr = new HandleBar();
-        double handlePrice = hbr.priceEvaluation();
-        Seating st = new Seating();
-        double seatingPrice = st.priceEvaluation();
-        Wheels wh = new Wheels();
-        double wheelPrice = wh.priceEvaluation();
+        Frame fr = null;
+        @SuppressWarnings("null")
+		double framePrice = fr.priceEvaluation();
+        HandleBar hbr = null;
+        @SuppressWarnings("null")
+		double handlePrice = hbr.priceEvaluation();
+        Seating st = null;
+        @SuppressWarnings("null")
+		double seatingPrice = st.priceEvaluation();
+        Wheels wh = null;
+        @SuppressWarnings("null")
+		double wheelPrice = wh.priceEvaluation();
 		double totalCost= chainPrice+framePrice+handlePrice+seatingPrice+wheelPrice;
 		return totalCost;
 	}
